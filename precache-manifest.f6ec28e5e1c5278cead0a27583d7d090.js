1bbeb25db8eecb4f5c37cf6af69a9210f797723b
vicky1234-dev <66732822+vicky1234-dev@users.noreply.github.com>
2020-10-14 14:57:07 +0530

    Updates

---
+++ precache-manifest.f6ec28e5e1c5278cead0a27583d7d090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30643c1ccc25cb44dd593846c117d01a",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "6511b7f2f088f124a63e",
+    "url": "/reactapp/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "8f3e7cf352ec73a695bb",
+    "url": "/reactapp/static/js/2.d99d3399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.d99d3399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6511b7f2f088f124a63e",
+    "url": "/reactapp/static/js/main.656d7f34.chunk.js"
+  },
+  {
+    "revision": "8c1959f69375e3823f96",
+    "url": "/reactapp/static/js/runtime-main.1f569a7a.js"
+  }
+]);
